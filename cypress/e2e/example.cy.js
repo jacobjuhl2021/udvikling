c32b4c21f19d358670322aacd76d314d59df8072
Jacob Juhl <jacobjuhlprivat@gmail.com>
2024-06-05 14:32:04 +0200

    6

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -1,7 +1,7 @@
 describe('frontend loads', () => {
   it('visits hte app', () => {
     // Visit the app - Ændre til den port din app kører på
-    cy.request('http://localhost:5174').then((response) => {
+    cy.request('http://localhost:5173').then((response) => {
       // Check if the app is running
       expect(response.status).to.eq(200)
       //npm run test:e2e:dev
@@ -11,7 +11,7 @@ describe('frontend loads', () => {
 
 describe('Check h1 tag on app has "You did it!"', () => {
   it('visits the app', () => {
-    cy.visit('http://localhost:5174')
+    cy.visit('http://localhost:5173')
     cy.get('h1').should('have.text', 'You did it!')
   })
 })
